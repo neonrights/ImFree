f3440c8dc4837b29b62bd20dfd3ae93dc00ae445
neonrights <nicholasfarn@gmail.com>
2015-10-18 03:13:57 -0700

    edited icon names (apparently android studio hates capital letters) and added to toolbar as imagebuttons

--- app/src/main/java/duplex/imfree/MainActivity.java
+++ app/src/main/java/duplex/imfree/MainActivity.java
@@ -17,6 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.friends);
+        toolbar.setNavigationIcon(R.drawable.settings);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
