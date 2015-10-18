05d948e5770f9a87be86b41f804f13b1ad5ff393
neonrights <nicholasfarn@gmail.com>
2015-10-18 07:10:43 -0700

    implemented basic availability toggle. Fucking android can't make toasts now. >;(

--- app/src/main/java/duplex/imfree/MainActivity.java
+++ app/src/main/java/duplex/imfree/MainActivity.java
@@ -1,22 +1,18 @@
 package duplex.imfree;
 
-import android.media.Image;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.content.Intent;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -26,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private ListView friendsList;
     private ArrayList<String> availableFriends;
     private ArrayAdapter<String> friendsAdapter;
+    FloatingActionButton availability;
+    boolean open = false; // determines if the user is currently available
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        availability = (FloatingActionButton) findViewById(R.id.fab);
+        availability.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
 
         refreshButton = (Button) findViewById(R.id.test_button);
 
@@ -58,17 +59,6 @@ public class MainActivity extends AppCompatActivity {
                 friendsAdapter.notifyDataSetChanged();
             }
         });
-
-        // toggle available/not button
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // TODO change availability status
-
-            }
-        });
-
     }
 
     @Override
@@ -108,5 +98,12 @@ public class MainActivity extends AppCompatActivity {
     // TODO updates how much time each person has left
     public void updateTime() {
         // dunno if void is needed
+    }
+
+    // availability button
+    public void toggleAvailability(View v) {
+        if (open) availability.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+        else availability.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+        open = !open;
     }
 }
